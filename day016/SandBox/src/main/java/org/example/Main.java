2466c38e94c78c8ecc625eb5347f2f0fc11cc187
fil777 <fil777@yandex.ru>
2026-04-06 11:47:27 +0300

    modified:   day016/SandBox/src/main/java/org/example/Main.java 	modified:   day016/SandBox/src/main/java/org/example/Person.java 	modified:   day018/SandBox018/src/main/java/stepik/course/Main.java 	modified:   day020/WebAppContacts/pom.xml 	deleted:    day020/WebAppContacts/src/main/java/com/example/ContactsApp.java 	deleted:    day020/WebAppContacts/src/main/java/com/example/controller/ContactController.java 	deleted:    day020/WebAppContacts/src/main/java/com/example/entity/Contact.java 	deleted:    day020/WebAppContacts/src/main/java/com/example/initiatedata/ContactsInitiator.java 	deleted:    day020/WebAppContacts/src/main/java/com/example/transliterator/CyrillicToLatin.java 	deleted:    day020/WebAppContacts/src/main/resources/application.properties

--- day016/SandBox/src/main/java/org/example/Main.java
+++ day016/SandBox/src/main/java/org/example/Main.java
@@ -3,6 +3,8 @@ package org.example;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
+import java.util.stream.IntStream;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -25,14 +27,23 @@ public class Main {
         System.out.println(pt.toString());
         System.out.println(pt);
 
-        ArrayList<Person> list = new ArrayList<>(){{
-                add (new Person(888, "vaska"));
-                add (new Person(111, "alka"));
-                add (new Person(555, "borka"));
+        ArrayList<Person> list = new ArrayList<>() {{
+            add(new Person(888, "vaska"));
+            add(new Person(111, "alka"));
+            add(new Person(555, "borka"));
         }};
 
         System.out.println(list);
         System.out.println(maxId(list));
+
+
+        List<Person> persons = new ArrayList<>();
+        IntStream.range(901, 903).forEach(i -> {
+            Person person = new Person(i, "person-" + i);
+            person.setName("new name " + i); // любые действия с person
+            persons.add(person);
+        });
+        System.out.println(persons);
     }
 
     public static int maxId(ArrayList<Person> people) {
--- day018/SandBox018/src/main/java/stepik/course/Main.java
+++ day018/SandBox018/src/main/java/stepik/course/Main.java
@@ -9,6 +9,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.time.LocalTime;
+
 import java.util.*;
 
 import static java.time.LocalTime.now;
